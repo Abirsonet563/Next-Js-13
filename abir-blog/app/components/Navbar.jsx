169a958cc1f4ca5b68e2d12197cbd46cc794c8bc
Abir Hasan <abirsonet563@gmail.com>
2024-11-22 20:31:37 +0000

    6. Style Font and Images

--- abir-blog/app/components/Navbar.jsx
+++ abir-blog/app/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Link from 'next/link'
-
+import Image from 'next/image'
+import Logo from "./dojo-logo.png"
 export default function Navbar() {
   return (
     <div>
          <nav>
+          <Image
+          src={Logo}
+          alt="Abir Help Desk Logo"
+          width={70}
+          quality={100}
+          placeholder='blur'
+          />
           <h1>Abir HelpDesk</h1>
           <Link href="/">Dashboard</Link>
           <Link href="/tickets">Tickets</Link>
